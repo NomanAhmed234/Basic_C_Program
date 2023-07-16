756861a358181550679dcce4146f060da8db977b
Noman Ahmed Khan <120250542+NomanAhmed234@users.noreply.github.com>
2023-07-16 16:39:28 +0500

    Add files via upload
    This Program will get the three number as a input from user and give the sum of these number.

---
+++ Q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{ 
+float a, b, c,sum; 
+	printf("Enter the first number: ");
+	scanf("%f",&a);
+	printf("Enter the second number: ");
+	scanf("%f",&b);
+	printf("Enter the third number: ");
+	scanf("%f",&c);
+	sum=a+b+c;
+	printf("Sum = %f",sum);	
+}
